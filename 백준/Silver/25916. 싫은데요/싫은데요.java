3845a303f0de4b286485b7592d60c9c8edd3881d
2sseul <109618184+2sseul@users.noreply.github.com>
2022-12-28 01:09:45 +0900

    [Silver I] Title: 싫은데요, Time: 456 ms, Memory: 66596 KB -BaekjoonHub

---
+++ 백준/Silver/25916. 싫은데요/싫은데요.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int holl[] = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			holl[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int left = 0, right = 1;
+		int sum = holl[0], ans = 0;
+	
+		while(right < N){
+			if(sum+holl[right] < M) {
+				sum += holl[right];
+				right++;
+				ans = Math.max(ans, sum);
+			}else if(sum+holl[right] == M) {
+				ans = sum+holl[right];
+				break;
+			}else {
+				sum -= holl[left];
+				left++;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
