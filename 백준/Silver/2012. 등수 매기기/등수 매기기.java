25fedeca4736b4dc2a7ec6f4548e5f10d7b3455e
2sseul <109618184+2sseul@users.noreply.github.com>
2023-01-10 21:55:15 +0900

    [Silver III] Title: 등수 매기기, Time: 912 ms, Memory: 50252 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[n];
+		
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		long result = 0;
+		for(int i=0;i<n;i++) {
+			result += Math.abs((i+1)-arr[i]);
+		}
+		System.out.println(result);
+		
+	}
+}
