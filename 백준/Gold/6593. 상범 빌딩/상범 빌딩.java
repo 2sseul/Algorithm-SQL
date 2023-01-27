a0a02f4b0a1870cf52d6a8862cb3ce87864f904c
2sseul <109618184+2sseul@users.noreply.github.com>
2023-01-27 09:32:53 +0900

    [Gold V] Title: 상범 빌딩, Time: 220 ms, Memory: 20268 KB -BaekjoonHub

---
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -0,0 +1,111 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int l, r, c;
+    static char[][][] building;
+    //동서남북상하
+    //동서남북은 층 안바뀌고, 상하만 층이 바뀜.
+    static int moveX[] = {0,0,0,0,1,-1};
+    static int moveR[] = {0,0,1,-1,0,0};
+    static int moveC[] = {1,-1,0,0,0,0};
+    static boolean visited[][][];
+    static class Node{
+        int x;
+        int y;
+        int z;
+        int canEscape;
+        Node(int x, int y, int z, int canEscape){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.canEscape = canEscape;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+    while(true) {
+        Queue<Node> queue = new LinkedList<>();
+        st = new StringTokenizer(br.readLine());
+        l = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        if(l==0&&r==0&&c==0) {
+            break;
+        }
+
+        building = new char[l][r][c];
+        visited = new boolean[l][r][c];
+        for(int i=0;i<l;i++) {
+            for(int j=0;j<r+1;j++) {
+                String s = br.readLine();
+                if(s.equals("")) {
+                    continue;
+                }
+                building[i][j]=s.toCharArray();
+            }
+        }
+
+        for(int i=0;i<l;i++) {
+            for(int j=0;j<r;j++) {
+                for(int k=0;k<c;k++) {
+                    if(building[i][j][k]=='S') {
+                    	visited[i][j][k] = true;
+                        queue.add(new Node(i,j,k,0));
+                        
+                    }
+                }
+            }
+        }
+        
+        boolean check = false;
+        while(!queue.isEmpty()) {
+            Node n = queue.poll();
+            
+            if(building[n.x][n.y][n.z]=='E') { 
+                if(n.canEscape>0) {
+                    check = true;
+                    System.out.println("Escaped in "+n.canEscape+" minute(s).");
+                    break;
+                }
+            }
+
+            for(int i=0;i<6;i++) {
+                int newL = n.x + moveX[i];
+                int newR = n.y + moveR[i];
+                int newC = n.z + moveC[i];
+
+                if(newL<0 || newL>=l || newR<0 || newR>=r || newC<0 || newC>=c) {
+                    continue;
+                }
+
+                if(building[newL][newR][newC]=='#') {
+                    continue;
+                }
+
+                if(visited[newL][newR][newC]) {
+                    continue;
+                }
+
+                if(building[newL][newR][newC]=='.' || building[newL][newR][newC]=='E' && !visited[newL][newR][newC]) {
+                    visited[newL][newR][newC] = true;
+                    queue.add(new Node(newL, newR, newC, n.canEscape+1));
+                }
+            }
+        }
+        
+        
+        if(!check) { //만약 도착지에 도달하지 못했다면? 이 추가가 되어야해
+            System.out.println("Trapped!");
+        }
+    }
+}
+}
