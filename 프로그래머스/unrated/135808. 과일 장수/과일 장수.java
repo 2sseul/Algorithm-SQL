a344c6683e16b1fa28d6b206cc04ec7ee9b4d031
2sseul <109618184+2sseul@users.noreply.github.com>
2023-02-23 09:33:27 +0900

    [unrated] Title: 과일 장수, Time: 0.34 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        
+        Arrays.sort(score);
+        
+        int length = score.length;
+        
+        int answer = 0;
+        
+        for(int i=length-m;i>=0;i-=m){
+            answer += score[i]*m;
+        }
+        
+        return answer;
+    }
+}
