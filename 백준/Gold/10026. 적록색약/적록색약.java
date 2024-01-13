93c73dce76ec0469dd8ed646059f297ab26811c4
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-01-14 02:04:36 +0900

    [Gold V] Title: 적록색약, Time: 648 ms, Memory: 103224 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,169 +1,119 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int N;
-	static char map[][];
-	static char mapRG[][];
+	static int num;
+	static int rgbNum;
+	static String map[][];
 	static boolean visited[][];
-	static boolean visitedRG[][];
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static boolean visitedRGB[][];
+	static int[] xR = {-1,1,0,0};
+	static int[] yC = {0,0,-1,1};
+	static class Node{
+		int x;
+		int y;
+		Node(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		N = Integer.parseInt(br.readLine());
 		
-		map = new char[N][N];
-		mapRG = new char[N][N];
-		
-		for(int i=0;i<N;i++) {
-			String s = br.readLine();
-			map[i] = s.toCharArray();
-			mapRG[i] = s.toCharArray();
-		}
-		
+		map = new String[N][N];
 		visited = new boolean[N][N];
-		
-		int cnt = 0;
-		int count = 0;
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(map[i][j]=='R'&&!visited[i][j]) {
-					pillR(i,j);
-					cnt++;
-				}
-				if(map[i][j]=='G'&&!visited[i][j]) {
-					pillG(i,j);
-					cnt++;
-				}
-				if(map[i][j]=='B'&&!visited[i][j]) {
-					pillB(i,j);
-					cnt++;
-				}
-			}
-		}
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(mapRG[i][j]=='G') {
-					mapRG[i][j]='R';
-				}
-			}
-		}
-		
-		visitedRG = new boolean[N][N];
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(mapRG[i][j]=='R'&&!visitedRG[i][j]) {
-					pillRG(i,j);
-					count++;
-				}
-				if(mapRG[i][j]=='B'&&!visitedRG[i][j]) {
-					pillRGB(i,j);
-					count++;
-				}
-			}
-		}
-		
-		System.out.println(cnt+" "+count);
-		
-		
-	}
+		visitedRGB = new boolean[N][N];
 
-	private static void pillRG(int x, int y) {
-		visitedRG[x][y] = true;
+		for(int i=0; i<N; i++) {
+			String tmp = br.readLine();
+			for(int j=0; j<N; j++) {
+				map[i] = tmp.split("");
+			}
+		}
 		
-		int xr[] = {-1,1,0,0};
-		int yc[] = {0,0,-1,1};
+		num = 0;
+		rgbNum = 0;
 		
-		for(int i=0;i<4;i++) {
-			int newR = x + xr[i];
-			int newC = y + yc[i];
-			
-			if(newR>=0 && newR<N && newC>=0 && newC<N) {
-				if(mapRG[newR][newC]=='R'&&!visitedRG[newR][newC]) {
-					pillRG(newR,newC);
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(map[i][j].equals("R") && !visited[i][j]) {
+					bfs(i, j, "G", "B");
+					num++;
+				}
+				else if(map[i][j].equals("G") && !visited[i][j]) {
+					bfs(i, j, "R", "B");
+					num++;
+				}else if(map[i][j].equals("B") && !visited[i][j]){
+					bfs(i, j, "R", "G");
+					num++;
+					rgbNum++;
 				}
 			}
 		}
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(map[i][j].equals("R") || map[i][j].equals("G")) {
+					if(!visitedRGB[i][j]) {
+						rgbBFS(i, j, "B");
+						rgbNum++;
+					}
+				}
+			}
+		}
+		
+		sb.append(num + " " + rgbNum);
+		System.out.println(sb.toString());
 		
 	}
 	
-	private static void pillRGB(int x, int y) {
-		visitedRG[x][y] = true;
+	static void bfs(int x, int y, String nonColor1, String nonColor2) {
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(new Node(x, y));
 		
-		int xr[] = {-1,1,0,0};
-		int yc[] = {0,0,-1,1};
-		
-		for(int i=0;i<4;i++) {
-			int newR = x + xr[i];
-			int newC = y + yc[i];
+		while(!queue.isEmpty()) {
+			Node node = queue.poll();
+			visited[node.x][node.y] = true;
 			
-			if(newR>=0 && newR<N && newC>=0 && newC<N) {
-				if(mapRG[newR][newC]=='B'&&!visitedRG[newR][newC]) {
-					pillRGB(newR,newC);
+			for(int i=0; i<4; i++) {
+				int newX = node.x + xR[i];
+				int newY = node.y + yC[i];
+				
+				if(newX < 0 || newX >= N || newY < 0 || newY >= N || visited[newX][newY] || map[newX][newY].equals(nonColor1) || map[newX][newY].equals(nonColor2)) {
+					continue;
 				}
+				
+				queue.add(new Node(newX, newY));
+				visited[newX][newY] = true;
 			}
+			
 		}
-		
 	}
 	
-	private static void pillR(int x, int y) {
-		visited[x][y] = true;
+	static void rgbBFS(int x, int y, String nonColor) {
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(new Node(x, y));
 		
-		int xr[] = {-1,1,0,0};
-		int yc[] = {0,0,-1,1};
-		
-		for(int i=0;i<4;i++) {
-			int newR = x + xr[i];
-			int newC = y + yc[i];
+		while(!queue.isEmpty()) {
+			Node node = queue.poll();
+			visitedRGB[node.x][node.y] = true;
 			
-			if(newR>=0 && newR<N && newC>=0 && newC<N) {
-				if(map[newR][newC]=='R'&&!visited[newR][newC]) {
-					pillR(newR,newC);
+			for(int i=0; i<4; i++) {
+				int newX = node.x + xR[i];
+				int newY = node.y + yC[i];
+				
+				if(newX < 0 || newX >= N || newY < 0 || newY >= N || visitedRGB[newX][newY] ||  map[newX][newY].equals(nonColor)) {
+					continue;
 				}
+				
+				queue.add(new Node(newX, newY));
+				visitedRGB[newX][newY] = true;
 			}
-		}
-		
-	}
-	
-	private static void pillG(int x, int y) {
-		visited[x][y] = true;
-		
-		int xr[] = {-1,1,0,0};
-		int yc[] = {0,0,-1,1};
-		
-		for(int i=0;i<4;i++) {
-			int newR = x + xr[i];
-			int newC = y + yc[i];
 			
-			if(newR>=0 && newR<N && newC>=0 && newC<N) {
-				if(map[newR][newC]=='G'&&!visited[newR][newC]) {
-					pillG(newR, newC);
-				}
-			}
 		}
-		
-	}
-	private static void pillB(int x, int y) {
-		visited[x][y] = true;
-		
-		int xr[] = {-1,1,0,0};
-		int yc[] = {0,0,-1,1};
-		
-		for(int i=0;i<4;i++) {
-			int newR = x + xr[i];
-			int newC = y + yc[i];
-			
-			if(newR>=0 && newR<N && newC>=0 && newC<N) {
-				if(map[newR][newC]=='B'&&!visited[newR][newC]) {
-					pillB(newR,newC);
-				}
-			}
-		}
-		
 	}
 }
