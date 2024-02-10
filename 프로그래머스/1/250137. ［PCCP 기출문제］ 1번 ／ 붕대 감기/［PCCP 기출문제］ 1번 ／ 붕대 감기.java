5fbc21b3228a4b6f95a0085adaa85f290e1f9662
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-02-10 13:38:36 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int last_attack = attacks[attacks.length-1][0];
+        int attack_cnt = 0;
+        int heal_cnt = 0;
+        int now_health = health;
+        int full_time = bandage[0];
+        int heal_sec = bandage[1];
+        int heal_bonus = bandage[2];
+        
+        for(int i=1; i<last_attack+1; i++){
+            //공격
+            if(attacks[attack_cnt][0] == i){
+                now_health -= attacks[attack_cnt][1];
+                attack_cnt++;
+                heal_cnt = 0;
+                if(now_health <= 0){
+                    return -1;
+                }
+            //힐
+            }else{
+                if(now_health < health){
+                    heal_cnt++;
+                    now_health += heal_sec;
+                    if(heal_cnt == full_time){
+                        heal_cnt = 0;
+                        now_health += heal_bonus;
+                    }
+                }else{
+                    heal_cnt++;
+                    now_health = health;
+                }
+
+            }
+        }
+        
+        if(now_health <= 0){
+            now_health = -1;
+        }
+        return now_health;
+    }
+}
