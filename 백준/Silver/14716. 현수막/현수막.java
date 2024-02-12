2da95bc8eaf49c1e71fc71f7158ca68b94792be8
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-02-13 00:10:31 +0900

    [Silver I] Title: 현수막, Time: 252 ms, Memory: 26112 KB -BaekjoonHub

--- 백준/Silver/14716. 현수막/현수막.java
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -8,14 +8,6 @@ public class Main {
 	static int cnt = 0;
 	static int goX[] = { -1, -1, -1, 0, 0, 1, 1, 1};
 	static int goY[] = {-1, 0, 1, -1, 1, -1, 0, 1};
-	static class Node{
-		int x;
-		int y;
-		Node(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
-	}
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -38,33 +30,28 @@ public class Main {
 			for(int j=0; j<M; j++) {
 				if(map[i][j]==1 && !visited[i][j]) {
 					cnt++;
-					BFS(i, j);
+					DFS(i, j);
 				}
 			}
 		}
 		
 		sb.append(cnt);
 		System.out.println(cnt);
-	}
-	static void BFS(int x, int y) {
-		Queue<Node> queue = new LinkedList<>();
-		queue.offer(new Node(x, y));
 		
-		while(!queue.isEmpty()) {
-			Node node = queue.poll();
-			visited[node.x][node.y] = true;
+	}
+	static void DFS(int x, int y) {
+		visited[x][y] = true;
+		
+		for(int i=0; i<8; i++) {
+			int newX = x + goX[i];
+			int newY = y + goY[i];
 			
-			for(int i=0; i<8; i++) {
-				int newX = node.x + goX[i];
-				int newY = node.y + goY[i];
-				
-				if(newX <0 || newX >= N || newY <0 || newY >=M || visited[newX][newY] || map[newX][newY] == 0) {
-					continue;
-				}
-				
-				queue.add(new Node(newX, newY));
-				visited[newX][newY] = true;
+			if(newX <0 || newX >= N || newY <0 || newY >=M || visited[newX][newY] || map[newX][newY] == 0) {
+				continue;
 			}
+			
+			DFS(newX, newY);
+			visited[newX][newY] = true;
 		}
 	}
 }
