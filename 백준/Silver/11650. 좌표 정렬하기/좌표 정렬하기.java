7dc40d77ab5edb9d78e9c9490269a9a5fe322582
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-03-02 18:21:10 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 788 ms, Memory: 57128 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,46 +1,50 @@
-
 import java.io.*;
 import java.util.*;
+
 
 public class Main {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int[][] map = new int[N][2];
-		
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=0; j<2; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		Arrays.sort(map, new Comparator<int[]>() {
-
-			@Override
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>() {
+			
 			public int compare(int[] o1, int[] o2) {
-				if(o1[0] == o2[0]) {
-					return o1[1]-o2[1];
+				if(o1[0] < o2[0]) {
+					return -1;
+				}else if(o1[0] > o2[0]) {
+					return 1;
+				}else {
+					if(o1[1] < o2[1]) {
+						return -1;
+					}else if(o1[1] > o2[1]) {
+						return 1;
+					}else {
+						return 0;
+					}
 				}
-				return o1[0]-o2[0];
 			}
 		});
 		
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<2; j++) {
-				sb.append(map[i][j]);
-				if(j!=1) {
-					sb.append(" ");
-				}
-			}
-			if(i!=N-1) {
-				sb.append('\n');
-			}
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int tc=0; tc<N; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			int[] tmp = {x, y};
+			
+			pq.offer(tmp);
 		}
 		
-		System.out.println(sb);
+		while(!pq.isEmpty()) {
+			int[] now = pq.poll();
+			sb.append(now[0]+" "+now[1]).append('\n');
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 }
