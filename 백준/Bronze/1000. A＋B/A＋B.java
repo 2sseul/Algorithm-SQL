ba857680c056c31a65dc5d59a2c32c5cd508796c
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-03-15 23:59:01 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        
+		int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        sb.append(A+B);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+	}
+}
