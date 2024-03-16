7be5f3d236a7be3ce537b4ab60a33dd2255f93bd
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-03-17 00:26:19 +0900

    [Silver II] Title: 가희와 키워드, Time: 1296 ms, Memory: 218560 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -1,37 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-	
-		st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		Map<String, Integer> map = new HashMap<>();
-		
-		for(int i=0;i<N;i++) {
-			String s = br.readLine();
-			map.put(s, i);
+		for(int i=0; i<N; i++) {
+			map.put(br.readLine(), 0);
 		}
 		
-		for(int i=0;i<M;i++) {
-			String s = br.readLine();
-			String arr[] = s.split(",");
-			for(int j=0;j<arr.length;j++) {
+		
+		for(int i=0; i<M; i++) {
+			String tmp = br.readLine();
+			String arr[] = tmp.split(",");
+			for(int j=0; j<arr.length; j++) {
 				if(map.containsKey(arr[j])) {
 					map.remove(arr[j]);
 				}else {
 					continue;
 				}
 			}
-			System.out.println(map.size());
+			sb.append(map.size()).append('\n');
 		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 }
