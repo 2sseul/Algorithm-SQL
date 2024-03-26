3f4237261136943b80a767c5d0f91d0181940e3c
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-03-26 23:58:07 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		
-		System.out.println(A*B);
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        
+		int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        sb.append(A*B);
+        bw.write(sb.toString());
+        bw.close();
 	}
 }
