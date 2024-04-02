ebafd11a3d89f8aaa34331f212930f255bb136da
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-04-03 00:20:57 +0900

    [Silver II] Title: 수열의 합, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1024. 수열의 합/수열의 합.java
+++ 백준/Silver/1024. 수열의 합/수열의 합.java
@@ -3,68 +3,45 @@ import java.util.*;
 
 public class Main {
 	static int N, L;
-	static long sum;
-	static int cnt, e, s;
-	static int min = Integer.MAX_VALUE;
-	static StringBuilder sb = new StringBuilder();
-	static TreeMap<Integer, int[]> map = new TreeMap<>();
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuilder sb = new StringBuilder();
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 		
-		sum = 0;
-		cnt = 0;
-		e = 0;
-		s = 0;
+		int idx = 0;
+		long sum = 0;
+		boolean flag = false;
+		for(int i=L; i<=100; i++) {
+			idx = (N/i) - (i-1)/2;
+			if(idx < 0) {
+				continue;
+			}
+			for(int j=idx; j<idx+i; j++) {
+				sb.append(j).append(" ");
+				sum += j;
+			}
+			if(sum == N) { 
+				flag = true;
+				break;
+			}else {
+				sum = 0;
+				sb.setLength(0);
+				continue;
+			}
+		}
 		
-		Check(0, 1);
 		
-		if(cnt != 0 && min <= 100 && min >= L) {
-			int tmp[] = map.get(min);
-			Out(tmp[1], tmp[0]);
-		}else {
+		
+		if(!flag) {
 			sb.append(-1);
 		}
 		
 		bw.write(sb.toString());
 		bw.close();
-		
-	}
-	static void Check(int start, int end) {
-		sum = start + end;
-		while(true) {
-			if(start >= end) {
-				return;
-			}
-			
-			if(sum < N) {
-				end++;
-				sum += end;
-			}else if(sum > N) {
-				sum -= start;
-				start++;
-			}else {
-				e = end;
-				s = start;
-				cnt = end - start + 1;
-				int tmp[] = {end, start};
-				if(cnt >= L) {
-					map.put(cnt, tmp);
-					min = Math.min(min, cnt);
-				}
-				sum -= start;
-				start++;
-			}
-			
-		}
-	}
-	static void Out(int start, int end) {
-		for(int i=start; i<=end; i++) {
-			sb.append(i).append(" ");
-		}
 	}
 }
