9a8a7427cffbc2db7b2f35b1ef2e9233b179de87
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-04-04 22:36:59 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18336 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,60 +1,29 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	static int N;
-	static int arr[];
-	static boolean visited[];
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringBuilder sb = new StringBuilder();
 		
-		N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		arr = new int[1000001];
-		visited = new boolean[1000001];
+		int dp[] = new int[N+1];
 		
-		Go(N);
-		
-		System.out.println(arr[1]);
-		
-	}
-	static void Go(int N) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.offer(N);
-		visited[N] = true;
-		
-		while(!queue.isEmpty()) {
-			int now = queue.poll();
-			int tmp = 0;
-			for(int i=0; i<3; i++) {
-				if(i == 0) {
-					if(now%3 == 0) {
-						tmp = now / 3;
-					}else {
-						continue;
-					}
-				}else if(i == 1) {
-					if(now%2 == 0) {
-						tmp = now / 2;
-					}else {
-						continue;
-					}
-				}else {
-					tmp = now - 1;
-				}
-				
-				if(tmp < 0 || visited[tmp]) {
-					continue;
-				}
-				
-				arr[tmp] = arr[now] + 1;
-				queue.offer(tmp);
-				visited[tmp] = true;
+		dp[1] = 0;
+		for(int i=2; i<=N; i++) {
+			dp[i] = dp[i-1] + 1;
+			if(i%2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2]+1);
+			}
+			if(i%3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3]+1);
 			}
 		}
- 		
+		
+		sb.append(dp[N]);
+		bw.write(sb.toString());
+		bw.close();
 	}
 }
