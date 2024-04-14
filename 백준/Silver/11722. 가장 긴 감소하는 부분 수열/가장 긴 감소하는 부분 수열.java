71079bc2be7ee44ecf27f3eabe1df1c95125dbb4
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-04-14 12:45:38 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 144 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[N];
+		int dp[] = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1;
+ 		}
+
+		int num = 1;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<i; j++) {
+				if(arr[i] < arr[j] && dp[i] < dp[j]+1) {
+					dp[i] = dp[j]+1;
+					num = Math.max(num, dp[i]);
+				}
+			}
+		}
+		
+		sb.append(num);
+		bw.write(sb.toString());
+		bw.close();
+	}
+}
