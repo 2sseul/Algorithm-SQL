d010a889b79dbb7ff9f6d7aa4ae33fbe3c344d1e
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-07-16 10:16:18 +0900

    [level 3] Title: 정수 삼각형, Time: 9.91 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int n = triangle.length;
+        int m = triangle[n-1].length;
+        
+        for(int i=triangle.length-1;i>0;i--){
+            for(int j=0;j<triangle[i].length-1;j++){
+                triangle[i-1][j]+=Math.max(triangle[i][j],triangle[i][j+1]);
+            }
+        }
+        
+        return triangle[0][0];
+    }
+}
