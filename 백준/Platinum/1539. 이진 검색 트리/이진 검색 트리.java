339348ecf1c7af70b6d291b1fc0cbd7bb62736ab
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2024-08-31 13:08:51 +0900

    [Platinum V] Title: 이진 검색 트리, Time: 732 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Platinum/1539. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Platinum/1539. 이진 검색 트리/이진 검색 트리.java
@@ -1,38 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        // 노드별로 높이를 기록할 배열
-        int[] len = new int[N];
-        long ans = 0;
-        TreeSet<Integer> tree = new TreeSet<>();
-
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (tree.higher(num) == null) {
-                if (tree.lower(num) == null) {
-                    len[num] = 1;
-                } else {
-                    len[num] = len[tree.lower(num)]+1;
-                }
-            } else {
-                if (tree.lower(num) == null) {
-                    len[num] = len[tree.higher(num)] + 1;
-                } else {
-                    len[num] = Math.max(len[tree.higher(num)], len[tree.lower(num)]) + 1;
-                }
-            }
-            ans += len[num];
-            tree.add(num);
-        }
-
-        System.out.println(ans);
-    }
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+		TreeSet<Integer> set = new TreeSet<>();
+		
+		long answer = 0;
+		for(int i=0; i<N; i++) {
+			int now = Integer.parseInt(br.readLine());
+			if(set.higher(now) == null) {
+				if(set.lower (now) == null) {
+					arr[now] = 1;
+				}else {
+					arr[now] = arr[set.lower (now)]+1;
+				}
+			}else {
+				if(set.lower(now) == null) {
+					arr[now] = arr[set.higher(now)]+1;
+				}else {
+					arr[now] = Math.max(arr[set.higher(now)], arr[set.lower(now)])+1;
+				}
+			}
+			set.add(now);
+			answer += arr[now];
+		}
+		
+		System.out.println(answer);
+	}
 }
