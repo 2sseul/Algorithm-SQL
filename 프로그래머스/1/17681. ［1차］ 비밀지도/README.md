890e894d9191292639d486088ae6f21f6bf02e7c
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2025-04-15 18:48:16 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=python3) 
 
 ### 성능 요약
 
-메모리: 87.5 MB, 시간: 20.50 ms
+메모리: 9.26 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 15일 18:42:36
+2025년 04월 15일 18:48:12
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,17 +2,8 @@ def solution(n, arr1, arr2):
     answer = []
     # 배열로 만들기
     for i in range(0, len(arr1)):
-        list1 = list(format(arr1[i], f'0{n}b'))
-        list2 = list(format(arr2[i], f'0{n}b'))
-        # 1인지 0인지 판단하고 지도 만들기
-        tmp = ''
-        for j in range(0, len(list1)):
-            if list1[j] == '1' or list2[j] == '1':
-                list1[j] = 1
-            tmp += str(list1[j])
-            
-        # 1은 #으로, 0은 공백으로 대치
-        tmp = tmp.replace('1','#',n)
-        tmp = tmp.replace('0',' ',n)
-        answer.append(tmp)
+        binary = format(arr1[i] | arr2[i], f'0{n}b')
+        binary = binary.replace('1','#',n)
+        binary = binary.replace('0',' ',n)
+        answer.append(binary)
     return answer
