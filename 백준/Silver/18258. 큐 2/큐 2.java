ba8f6c30323b64f69e538da13883995b8af921af
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2026-03-22 22:46:07 -0700

    [Silver IV] Title: 큐 2, Time: 1568 ms, Memory: 408404 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -2,68 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static Deque<Integer> queue = new LinkedList<>();
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int tc=0; tc<N; tc++) {
-			String tmp = br.readLine();
-			String arr[] = tmp.split(" ");
-			
-			if(arr[0].equals("push")) {
-				Push(Integer.parseInt(arr[1]));
-			}else if(arr[0].equals("front")) {
-				Front();
-			}else if(arr[0].equals("back")) {
-				Back();
-			}else if(arr[0].equals("size")) {
-				Size();
-			}else if(arr[0].equals("pop")) {
-				Pop();
-			}else if(arr[0].equals("empty")) {
-				Empty();
-			}
-		}
-		
-		bw.write(sb.toString());bw.flush();bw.close();
-	}
-	static void Empty() {
-		if(!queue.isEmpty()) {
-			sb.append(0).append('\n');
-		}else {
-			sb.append(1).append('\n');
-		}
-	}
-	static void Pop() {
-		if(!queue.isEmpty()) {
-			int num = queue.poll();
-			sb.append(num).append('\n');
-		}else {
-			sb.append(-1).append('\n');
-		}
-	}
-	static void Size() {
-		sb.append(queue.size()).append('\n');
-	}
-	static void Back() {
-		if(!queue.isEmpty()) {
-			sb.append(queue.getLast()).append('\n');
-		}else {
-			sb.append(-1).append('\n');
-		}
-	}
-	static void Front() {
-		if(!queue.isEmpty()) {
-			sb.append(queue.getFirst()).append('\n');
-		}else {
-			sb.append(-1).append('\n');
-		}
-	}
-	static void Push(int x) {
-		queue.add(x);
-	}
+    static Deque<Integer> q = new LinkedList<>();
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0; i<n; i++){
+            String[] tmp = br.readLine().split(" ");
+            String order = tmp[0];
+            int num = 0;
+            if(tmp.length >= 2) {
+                num = Integer.parseInt(tmp[1]);
+            }
+
+            if(order.equals("push")){
+                q.add(num);
+            }else if(order.equals("pop")){
+                if(!q.isEmpty()){
+                    int popNum = q.poll();
+                    sb.append(popNum).append('\n');
+                }else{
+                    sb.append(-1).append('\n');
+                }
+            }else if(order.equals("size")){
+                sb.append(q.size()).append('\n');
+            }else if(order.equals("empty")){
+                if(!q.isEmpty()){
+                    sb.append(0).append('\n');
+                }else{
+                    sb.append(1).append('\n');
+                }
+            }else if(order.equals("front")){
+                if(!q.isEmpty()){
+                    sb.append(q.peekFirst()).append('\n');
+                }else{
+                    sb.append(-1).append('\n');
+                }
+            }else if(order.equals("back")){
+                if(!q.isEmpty()){
+                    sb.append(q.peekLast()).append('\n');
+                }else{
+                    sb.append(-1).append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
 }
