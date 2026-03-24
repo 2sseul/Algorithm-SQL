a20ccbbc87a0bb33b40a5b373d7056651ad3a9c5
Iseul Kim <109618184+2sseul@users.noreply.github.com>
2026-03-23 22:39:08 -0700

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,45 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		Stack<Character> stack = new Stack<>();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int tc = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<tc; i++) {
-			stack.clear();
-			
-			String tmp = br.readLine();
-			//System.out.println(tmp);
-			String answer = "YES";
-			
-			for(int j=0; j<tmp.length(); j++) {
-				if(stack.isEmpty()) {
-					stack.push(tmp.charAt(j));
-				}else {
-					char check = stack.peek();
-					if(check == '(') {
-						if(tmp.charAt(j)==')') {
-							stack.pop();
-						}else {
-							stack.push(tmp.charAt(j));
-						}
-					}else {
-						stack.push(tmp.charAt(j));
-					}
-				}
-			}
-			
-			if(!stack.isEmpty()) {
-				answer = "NO";
-			}
-			sb.append(answer).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-		
-	}
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<T; i++){
+            String sentence = br.readLine();
+            for(int j=0; j<sentence.length(); j++){
+
+                if(stack.isEmpty()){
+                    stack.push(sentence.charAt(j));
+                }else if(stack.peek() == '(' && sentence.charAt(j) == '('){
+                    stack.push(sentence.charAt(j));
+                }else{
+                    char tmp = stack.peek();
+                    if(tmp == '(' && sentence.charAt(j) == ')'){
+                        stack.pop();
+                    }
+                }
+            }
+            if(stack.size() == 0){
+                sb.append("YES").append('\n');
+            }else{
+                sb.append("NO").append('\n');
+            }
+            stack.clear();
+        }
+
+        System.out.println(sb);
+    }
 }
